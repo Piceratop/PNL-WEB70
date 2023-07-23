af5074b5c302a051c1ad7abe90f1518378a7a2f8
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-07-23 21:26:55 +0700

    req params

--- 2023-07-23/app.js
+++ 2023-07-23/app.js
@@ -1,3 +1,30 @@
 import express from "express";
+import crypto from "crypto";
 
 const app = express();
+
+const datas = [
+    {
+        id: crypto.randomUUID(),
+        name: "Alice",
+        age: 10,
+    },
+];
+
+app.get("/data/", (req, res) => {
+    res.send(datas);
+});
+
+app.get("/data/:id", (req, res) => {
+    const { id } = req.params;
+    const data = datas.find((data) => data.id === id);
+    if (data) {
+        res.send(data);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
