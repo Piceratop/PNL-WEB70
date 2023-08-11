eacd732e623c76d6917695fcacdb6dc4e02739a7
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2023-08-11 10:28:06 +0700

    Finish homework

--- 2023-08-09/homework/ex2/routes/post.route.js
+++ 2023-08-09/homework/ex2/routes/post.route.js
@@ -60,4 +60,87 @@ postRouter.put("/:id", async (req, res) => {
     }
 });
 
+postRouter.get("/", async (req, res) => {
+    try {
+        if (Object.keys(req.query).length === 0) {
+            const posts = await dbCollection["posts"].find().toArray();
+            resClientData(res, 200, posts, "Success");
+            return;
+        }
+        const { field, gt } = req.query;
+        if (field === "liked") {
+            const posts = await dbCollection["posts"]
+                .find({ liked: { $gt: parseInt(gt) } })
+                .toArray();
+            resClientData(res, 200, posts, "Success");
+            return;
+        }
+        resClientData(res, 200, null, "Success");
+        return;
+    } catch (error) {
+        resClientData(res, 500, null, error.message);
+        return;
+    }
+});
+
+postRouter.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await dbCollection["posts"].find({ id }).toArray();
+        if (post.length === 0) {
+            resClientData(res, 404, null, "Post not found");
+            return;
+        }
+        resClientData(res, 200, post, "Success");
+        return;
+    } catch (error) {
+        resClientData(res, 500, null, error.message);
+        return;
+    }
+});
+
+postRouter.post("/like/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await dbCollection["posts"].find({ id }).toArray();
+        if (post.length === 0) {
+            resClientData(res, 404, null, "Post not found");
+            return;
+        }
+
+        const liked = post[0].liked || 0;
+        await dbCollection["posts"].updateOne(
+            { id },
+            { $set: { liked: liked + 1 } }
+        );
+        resClientData(res, 200, null, "Success");
+        return;
+    } catch (error) {
+        resClientData(res, 500, null, error.message);
+        return;
+    }
+});
+
+postRouter.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await dbCollection["posts"].find({ id }).toArray();
+        if (post.length === 0) {
+            resClientData(res, 404, null, "Post not found");
+            return;
+        }
+        if (post[0].userId !== req.userId) {
+            resClientData(res, 401, null, "Unauthorized");
+            return;
+        }
+        console.log(id);
+        await dbCollection["posts"].deleteOne({ id });
+        resClientData(res, 200, null, "Success");
+        return;
+    } catch (error) {
+        resClientData(res, 500, null, error.message);
+        return;
+    }
+});
+
 export default postRouter;
